697cd92c4baa6b9a4f5ec6ec60e103948bbe8b2b
Lhimor-Entino <lhimortinoen007@gmail.com>
2020-02-10 11:58:52 +0800

    Test

---
+++ src/shapeprojectfxml/FXMLShapeControllerController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shapeprojectfxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author MIC-P ICT
+ */
+public class FXMLShapeControllerController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
